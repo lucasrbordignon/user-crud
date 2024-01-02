d40240428785e72f6f9df93dcc9adab639cb2237
lucasrbordigon <lucas.r.bordignon@gmail.com>
2024-01-02 11:02:54 -0300

    set db connection variebles in .env

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
 
 async function main() {
+  require('dotenv').config();
 
   try {
     mongoose.set('strictQuery', true)
 
     await mongoose.connect(
-      'mongodb+srv://lucasrbordignon:xk2zyrkMO7QcY42V@user-crud.assez7n.mongodb.net/?retryWrites=true&w=majority'
+      `mongodb+srv://${process.env.USER_MONGODB}:${process.env.PASSWORD_MONGODB}@user-crud.assez7n.mongodb.net/?retryWrites=true&w=majority`
     )
 
     console.log('Conectado ao banco!')
